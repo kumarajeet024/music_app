2a939a411e0bd5b73b0914d4cbc4a896a0420c31
kumarajeet024 <kumarajeet024@gmail.com>
2018-11-20 23:11:14 +0530

    another view

--- music/urls.py
+++ music/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     #/music/
     url(r'^$', views.index, name='index'),
     #/music/id/
-
-    url(r'^(?P<album_id>[0-9])$'),
+    # ^ represents beginning and $ represents end
+    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name = "detail"),
 ]
--- music/views.py
+++ music/views.py
@@ -2,6 +2,16 @@ from django.shortcuts import render
 
 
 from django.http import HttpResponse
+from .models import Album
 
 def index(request):
-    return HttpResponse('<h1>This is our music website</h1>')
+    all_albums = Album.objects.all()
+    html = ''
+    for album in all_albums:
+        url = '/music/' +str(album.id)+ '/'
+        html += '<a href="'+url+'">' + album.album_title + '</a><br>'
+    return HttpResponse(html)
+
+
+def detail(request, album_id):
+    return HttpResponse("<h2>Details for album id:" + str(album_id)+"</h2>")
