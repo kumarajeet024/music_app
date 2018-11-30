73ad62f05947d446db41cbf4d2a635ec40a23611
kumarajeet024 <kumarajeet024@gmail.com>
2018-12-01 02:42:37 +0530

    design

--- music/admin.py
+++ music/admin.py
@@ -2,4 +2,6 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Album
+from .models import Song
 admin.site.register(Album)
+admin.site.register(Song)
--- music/models.py
+++ music/models.py
@@ -19,3 +19,6 @@ class Song(models.Model):
     #on_delete = models.CASCADE means on deleting the album the songs linked to them gets deleted.
     file_type = models.CharField(max_length=10)
     song_title = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.song_title
